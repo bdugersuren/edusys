a7faca54eb5c99651ef2cc15050f943403f27f02
munkhbayar.n <mgl0814@gmail.com>
2021-04-16 11:29:31 +0800

    taskbarselectAnswer

--- src/components/TasksComp/TaskItem.js
+++ src/components/TasksComp/TaskItem.js
@@ -4,8 +4,6 @@ import { Checkbox } from "antd";
 import { Row, Col, Divider } from "antd";
 import styles from "./style.module.css";
 
-
-
 import {
   CgMaximize,
   CgMinimize,
@@ -19,13 +17,6 @@ import {
   BiArrowFromTop,
   BiSortUp,
 } from "react-icons/bi";
-
-
-const radioStyle = {
-  display: "block",
-  height: "50px",
-  lineHeight: "50px",
-};
 
 function TaskItem({ task, setSelectedTasks }) {
   const [isAnswer, setIsAnswer] = useState(true);
@@ -41,30 +32,27 @@ function TaskItem({ task, setSelectedTasks }) {
   return (
     <Row>
       <Col xs={21} sm={21} md={21} lg={21} xl={21}>
-
-
-
-
-        <div className={styles.TaskQuestionHeader}  >
+        <div className={styles.TaskQuestionHeader}>
           <Checkbox onChange={onChange}></Checkbox>
-          
-          <div className={styles.TaskQuestionHeaderTitle}>{task.title}</div>      
-          
-        </div>
-      
-        </Col>
 
-        
+          <div className={styles.TaskQuestionHeaderTitle}>{task.title}</div>
+        </div>
+      </Col>
+
       <Col xs={3} sm={3} md={3} lg={3} xl={3}>
         <div className={styles.TaskQuestionHeader}>
-        <div>
+          <div>
             <BiSortUp className={styles.TaskListIcons} />
           </div>
           <div>
-            <BiArrowFromTop className={styles.TaskListIcons}/>
+            <BiArrowFromTop className={styles.TaskListIcons} />
           </div>
           <div onClick={() => setIsAnswer(!isAnswer)}>
-            {isAnswer ? <CgMinimize className={styles.TaskListIcons}/> : <CgMaximize className={styles.TaskListIcons}/>}
+            {isAnswer ? (
+              <CgMinimize className={styles.TaskListIcons} />
+            ) : (
+              <CgMaximize className={styles.TaskListIcons} />
+            )}
           </div>
           <div onClick={() => setIsQuestions(!isQuestions)}>
             {isQuestions ? (
@@ -74,11 +62,7 @@ function TaskItem({ task, setSelectedTasks }) {
             )}
           </div>
 
-
-
-
-
-{/* 
+          {/* 
 
           
           <div>
@@ -98,32 +82,26 @@ function TaskItem({ task, setSelectedTasks }) {
             )}
           </div> */}
         </div>
-
-        
       </Col>
 
+      {isQuestions && (
+        <div>
+          <div className={styles.TaskQuestionsList}>{task.questions}</div>
 
-
-
-
-
-        {isQuestions && (
-          <div >
-            <div className="">{task.questions}</div>
-
+          <div className={styles.TaskAnswerList}>
             {isAnswer && (
               <Radio.Group onChange={onChange} value={value}>
                 {task.q_answer.length > 0 &&
                   task.q_answer.map((ans) => (
-                    <Radio key={ans._id} style={radioStyle} value={ans._id}>
+                    <Radio className={styles.TaskAnswerRadio} key={ans._id}  value={ans._id}>
                       {ans.answer1}
                     </Radio>
                   ))}
               </Radio.Group>
             )}
           </div>
-        )}
-      
+        </div>
+      )}
     </Row>
   );
 }
